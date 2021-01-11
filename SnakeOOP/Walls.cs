45a32354ec4a123cb7af905cca675d6e62359163
Julia Aasmaa <my_e_10@hotmail.com>
2021-01-11 12:45:43 +0200

    commit

---
+++ SnakeOOP/Walls.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeOOP
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            HorizontalLine bottom = new HorizontalLine(0, 80, 25, '$');
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+
+            VerticalLine obstacle = new VerticalLine(10, 13, 50, '%');
+
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+            wallList.Add(obstacle);
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+
+        public bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
